2fd242d0392660074c457b4ccf655d8097148b0a
Ayub Khoirudin <khoirudinayub971@gmail.com>
2025-10-27 08:31:22 +0700

    add gui

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,9 +1,11 @@
-const prompt = require('prompt-sync')({ sigint: true })
-
 let humanScore = 0
 let computerScore = 0
 let draw = 0
 const arr = ['rock', 'paper', 'scissors']
+let round = 5
+const allButton = [...document.querySelectorAll('button')]
+const buttonContainer = document.querySelector('.button-container')
+const result = document.querySelector('.result')
 
 function random(array) {
   return Math.floor(Math.random() * array.length)
@@ -13,59 +15,68 @@ function getComputerChoice() {
   return arr[random(arr)]
 }
 
-function getHumanChoice() {
-  const choice = prompt("Your choice 'rock' or 'paper' or 'scissors': ")
-  return choice.toLowerCase()
+function getHumanChoice(val) {
+  return val.value
 }
 
 function firstCharUpper(word) {
   return word.slice(0, 1).toUpperCase() + word.slice(1)
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    draw++
-    console.log(
-      '=================================================\nDraw\n=================================================\n'
-    )
-  } else if (
-    (humanChoice === 'rock' && computerChoice === 'paper') ||
-    (humanChoice === 'paper' && computerChoice === 'scissors') ||
-    (humanChoice === 'scissors' && computerChoice === 'rock')
-  ) {
-    computerScore++
-    console.log(
-      `=================================================\nYou lose! ${firstCharUpper(
-        computerChoice
-      )} beats ${firstCharUpper(
-        humanChoice
-      )}\n=================================================\n`
-    )
+function reset() {
+  window.location.reload()
+}
+
+function showScore() {
+  alert(
+    `SCORE:\nUSER(${humanScore})    COMP(${computerScore})    DRAW(${draw})\n`
+  )
+}
+
+function showResult() {
+  if (humanScore > computerScore) {
+    result.textContent = 'YOU WIN!'
+  } else if (humanScore < computerScore) {
+    result.textContent = 'YOU LOSE!'
   } else {
-    humanScore++
-    console.log(
-      `=================================================\nYou win! ${firstCharUpper(
-        humanChoice
-      )} beats ${firstCharUpper(
+    result.textContent = 'DRAW'
+  }
+  setTimeout(showScore, 1000)
+}
+
+function playRound(objButton) {
+  const humanChoice = getHumanChoice(objButton)
+  const computerChoice = getComputerChoice()
+
+  if (round != 0) {
+    if (humanChoice === computerChoice) {
+      draw++
+      result.textContent = 'Draw'
+    } else if (
+      (humanChoice === 'rock' && computerChoice === 'paper') ||
+      (humanChoice === 'paper' && computerChoice === 'scissors') ||
+      (humanChoice === 'scissors' && computerChoice === 'rock')
+    ) {
+      computerScore++
+      result.textContent = `You lose! ${firstCharUpper(
         computerChoice
-      )}\n=================================================\n`
-    )
+      )} beats ${firstCharUpper(humanChoice)}`
+    } else {
+      humanScore++
+      result.textContent = `You win! ${firstCharUpper(
+        humanChoice
+      )} beats ${firstCharUpper(computerChoice)}`
+    }
+    round--
+  }
+
+  // add reset button
+  if (round == 0) {
+    allButton.forEach(button => button.remove())
+    const resetButton = document.createElement('button')
+    resetButton.textContent = 'Reset'
+    buttonContainer.appendChild(resetButton)
+    resetButton.addEventListener('click', reset)
+    setTimeout(showResult, 2000)
   }
 }
-
-function playGame(rounds) {
-  while (rounds != 0) {
-    rounds--
-    console.log(
-      `Score:\nuser(${humanScore})    comp(${computerScore})    draw(${draw})\n`
-    )
-    playRound(getHumanChoice(), getComputerChoice())
-  }
-  if (rounds == 0) {
-    console.log(
-      `Score:\nuser(${humanScore})    comp(${computerScore})    draw(${draw})\n`
-    )
-  }
-}
-
-playGame(5)
