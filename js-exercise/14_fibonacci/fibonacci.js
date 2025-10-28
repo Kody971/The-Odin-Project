bc7d8c9316eddb8e4f7f3208b08c7c9825cd8990
Ayub Khoirudin <khoirudinayub971@gmail.com>
2025-10-28 09:35:26 +0700

    finish execise 14

--- js-exercise/14_fibonacci/fibonacci.js
+++ js-exercise/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+  if (num == 0) {
+    return 0
+  } else if (num < 0) {
+    return 'OOPS'
+  }
+  let base = [1, 1]
+  for (let i = 1, length = num - 1; i < length; i++) {
+    base.push(base[i] + base[i - 1])
+  }
+  return base[base.length - 1]
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
