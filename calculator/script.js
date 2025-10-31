659a2c97f23ee038b5a750bf6af2dc1a25ea85f5
Ayub Khoirudin <khoirudinayub971@gmail.com>
2025-10-31 18:04:28 +0700

    finish functionality calculator

--- calculator/script.js
+++ calculator/script.js
@@ -3,32 +3,44 @@ const num1 = document.querySelector('.num1')
 const num2 = document.querySelector('.num2')
 
 const reset = () => {
-  // do something
+  window.location.reload()
 }
 
 const del = () => {
-  // do something
+  if (num2.textContent == 'Infinity') {
+    return reset()
+  }
+  num2.textContent = num2.textContent.slice(0, -1)
 }
 
 const division = (...val) => {
-  num1.textContent = parseInt(val[0]) / parseInt(val[1])
+  return val[0] / val[1]
 }
 
 const multiply = (...val) => {
-  num1.textContent = parseInt(val[0]) * parseInt(val[1])
+  return val[0] * val[1]
 }
 
 const addition = (...val) => {
-  num1.textContent = parseInt(val[0]) + parseInt(val[1])
+  return val[0] + val[1]
 }
 
 const subtract = (...val) => {
-  num1.textContent = parseInt(val[0]) - parseInt(val[1])
+  return val[0] - val[1]
 }
 
 const checkArithmetic = val => {
+  if (num2.textContent == 'Infinity') {
+    return reset()
+  }
   if (arithmetic.textContent && num1.textContent && num2.textContent) {
-    return result(val.value)
+    result(val)
+    arithmetic.textContent = val.value
+    return
+  }
+  if (arithmetic.textContent) {
+    arithmetic.textContent = val.value
+    return
   }
   arithmetic.textContent = val.value
   num1.textContent = num2.textContent
@@ -37,40 +49,81 @@ const checkArithmetic = val => {
 }
 
 const userInput = val => {
+  if (num2.textContent == 'Infinity') {
+    return reset()
+  }
+  if (num2.textContent.includes('.') && val.value == '.') {
+    return
+  }
+  if (num2.textContent.length > 10) {
+    num2.textContent = Number(num2.textContent + val.value).toExponential()
+    return
+  }
   num2.textContent += val.value
 }
 
 const getNumber = () => {
-  return [num1.textContent, num2.textContent]
+  return [Number(num1.textContent), Number(num2.textContent)]
+}
+
+const resultProperly = val => {
+  console.log(val.length)
+  if (val.toString().length > 10) {
+    return val.toExponential()
+  }
+  return val
 }
 
 const result = val => {
-  if (val == 'total') {
-    console.log('=')
+  if (num2.textContent == 'Infinity') {
+    return reset()
+  }
+  if (val.value == 'total') {
+    if (!num2.textContent) {
+      num2.textContent = num1.textContent
+      arithmetic.textContent = ''
+      num1.textContent = ''
+    }
     switch (arithmetic.textContent) {
       case '/':
-        num2.textContent = division(getNumber())
+        arithmetic.textContent = ''
+        num2.textContent = resultProperly(division(...getNumber()))
         num1.textContent = ''
+        break
       case 'x':
-        num2.textContent = multiply(getNumber())
+        arithmetic.textContent = ''
+        num2.textContent = resultProperly(multiply(...getNumber()))
         num1.textContent = ''
+        break
       case '+':
-        num2.textContent = addition(getNumber())
+        arithmetic.textContent = ''
+        num2.textContent = resultProperly(addition(...getNumber()))
         num1.textContent = ''
+        break
       case '-':
-        num2.textContent = subtract(getNumber())
+        arithmetic.textContent = ''
+        num2.textContent = resultProperly(subtract(...getNumber()))
         num1.textContent = ''
+        break
     }
   } else {
     switch (arithmetic.textContent) {
       case '/':
-        num1.textContent = division(getNumber())
+        num1.textContent = resultProperly(division(...getNumber()))
+        num2.textContent = ''
+        break
       case 'x':
-        num1.textContent = multiply(getNumber())
+        num1.textContent = resultProperly(multiply(...getNumber()))
+        num2.textContent = ''
+        break
       case '+':
-        num1.textContent = addition(getNumber())
+        num1.textContent = resultProperly(addition(...getNumber()))
+        num2.textContent = ''
+        break
       case '-':
-        num1.textContent = subtract(getNumber())
+        num1.textContent = resultProperly(subtract(...getNumber()))
+        num2.textContent = ''
+        break
     }
   }
 }
