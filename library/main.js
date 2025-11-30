21b6619d2948deb3c4e57756079a4eda9840a768
Ayub Khoirudin <khoirudinayub971@gmail.com>
2025-11-30 12:11:56 +0700

    Add library project

---
+++ library/main.js
@@ -0,0 +1,117 @@
+let myLibrary = [
+  {
+    id: 1,
+    name: "The End of the World Volume 1",
+    author: "Acumalaka",
+    pages: 144,
+    read: true,
+  },
+  {
+    id: 2,
+    name: "The End of the World Volume 2",
+    author: "Acumalaka",
+    pages: 384,
+    read: false,
+  },
+];
+
+function Book(name, author, pages, readed) {
+  this.id = crypto.randomUUID();
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = readed;
+}
+
+const loadPage = () => {
+  const main = document.querySelector("main");
+  main.innerHTML = "";
+  showLibrary(myLibrary);
+};
+
+const addBookToLibrary = (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const title = form.elements.title.value;
+  const author = form.elements.author.value;
+  const pages = form.elements.pages.value;
+  const readed = form.elements.read.checked;
+
+  const newBook = new Book(title, author, pages, readed);
+  myLibrary.push(newBook);
+
+  loadPage();
+  form.reset();
+};
+
+const removeBookFromLibrary = (event) => {
+  event.preventDefault();
+
+  const card = event.target.closest(".card");
+  const id = card.querySelector("#userId").value;
+  myLibrary.splice(
+    myLibrary.findIndex((book) => book.id == id),
+    1
+  );
+
+  loadPage();
+};
+
+const showLibrary = (arr) => {
+  arr.forEach((book) => {
+    const main = document.querySelector("main");
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const id = document.createElement("input");
+    id.setAttribute("type", "hidden");
+    id.setAttribute("id", "userId");
+    id.setAttribute("name", "userId");
+    id.setAttribute("value", book.id);
+    card.appendChild(id);
+
+    const container1 = document.createElement("div");
+    container1.classList.add("container-detail");
+
+    const mark = document.createElement("p");
+    mark.classList.add("mark");
+    if (book.read) {
+      mark.classList.add("true");
+      mark.textContent = "Readed";
+    } else {
+      mark.classList.add("false");
+      mark.textContent = "Unreaded";
+    }
+    container1.appendChild(mark);
+
+    const name = document.createElement("h2");
+    name.textContent = book.name;
+    container1.appendChild(name);
+
+    const author = document.createElement("p");
+    author.textContent = book.author;
+    container1.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.textContent = book.pages + " pages";
+    container1.appendChild(pages);
+
+    const container2 = document.createElement("div");
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-btn");
+    removeBtn.textContent = "Remove";
+    removeBtn.addEventListener("click", removeBookFromLibrary);
+    container2.appendChild(removeBtn);
+
+    card.appendChild(container1);
+    card.appendChild(container2);
+    main.appendChild(card);
+  });
+};
+
+const addBtn = document.querySelector("form");
+addBtn.addEventListener("submit", addBookToLibrary);
+
+loadPage();
